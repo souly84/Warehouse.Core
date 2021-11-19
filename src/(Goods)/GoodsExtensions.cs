8981e58e4afa1660ae58f86aca9a617d3b45a9d2
Denis Zhukovski <dzhukovsky@softek.by>
2021-11-19 20:23:57 +0300

    extends goods entities with extra methods and classes

---
+++ src/(Goods)/GoodsExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Warehouse.Core
+{
+    public static class GoodsExtensions
+    {
+        public static async Task<IEnumerable<IGood>> WhereAsync(
+            this IGoods goods,
+            Func<IGood, bool> predicate)
+        {
+            var goodsList = await goods.ToListAsync();
+            return goodsList.Where(predicate);
+        }
+
+        public static Task<IEnumerable<IGood>> ByBarcodeAsync(
+            this IGoods goods,
+            string barcode)
+        {
+            return goods.WhereAsync((good) => good.Equals(barcode));
+        }
+    }
+}
--- src/(Goods)/IGood.cs
+++ src/(Goods)/IGood.cs
@@ -1,6 +1,23 @@
-﻿namespace Warehouse.Core
+﻿using MediaPrint;
+
+namespace Warehouse.Core
 {
-    public interface IGood
+    public interface IGood : IPrintable
     {
+    }
+
+    public class MockGood : IGood
+    {
+        private readonly string _id;
+
+        public MockGood(string id)
+        {
+            _id = id;
+        }
+
+        public void PrintTo(IMedia media)
+        {
+            media.Put("Id", _id);
+        }
     }
 }
