748948ac67c2654e0deb9a01d627abe9960d2ea8
Denis Zhukovski <dzhukovsky@softek.by>
2021-11-21 14:26:37 +0300

    Add unit tests for cached goods

---
+++ tests/(Extensions)/OneSuccessfulCallGoods.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Warehouse.Core.Tests.Extensions
+{
+    public class OneSuccessfulCallGoods : IGoods
+    {
+        private bool _wasCalled;
+        public Task<IList<IGood>> ToListAsync()
+        {
+            if (_wasCalled)
+            {
+                throw new InvalidOperationException("The method has been called twice");
+            }
+            _wasCalled = true;
+            return Task.FromResult<IList<IGood>>(new List<IGood>
+            {
+                new MockGood("1", 1),
+                new MockGood("2", 1),
+            });
+        }
+
+        public IGoods With(IFilter filter)
+        {
+            return new OneSuccessfulCallGoods();
+        }
+    }
+}
