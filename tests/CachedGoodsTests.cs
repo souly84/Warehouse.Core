1eafb7527cb7cf3423bade46760b9ba6550ce023
Denis Zhukovski <36443640+DenisZhukovsky@users.noreply.github.com>
2021-11-21 14:32:30 +0300

    Suppliers (#16)
    * cached goods entity
    
    * Add suppliers interfaces
    
    * Add unit tests for cached goods
    
    Co-authored-by: Denis Zhukovski <dzhukovsky@softek.by>

---
+++ tests/CachedGoodsTests.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using Warehouse.Core.Tests.Extensions;
+using Xunit;
+
+namespace Warehouse.Core.Tests
+{
+    public class CachedGoodsTests
+    {
+        [Fact]
+        public async Task CachesToListAsync()
+        {
+            var cached = new OneSuccessfulCallGoods().Cached();
+            Assert.NotEmpty(await cached.ToListAsync());
+            Assert.NotEmpty(await cached.ToListAsync());
+        }
+    }
+}
