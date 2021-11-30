5d2d4d38062b4b85b60e2f6b0bbfcaa854f0f4a8
Denis Zhukovski <dzhukovsky@softek.by>
2021-11-30 10:12:30 +0300

    reduce code duplication

---
+++ src/(Receptions)/(Confirmation)/Confirmation.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Warehouse.Core.Goods;
+
+namespace Warehouse.Core.Receptions
+{
+    /// <summary>
+    /// This class is needed to reduce code duplication. Otherwise the PR is rejected by Sonar cloud.
+    /// </summary>
+    public abstract class Confirmation : IConfirmation
+    {
+        private readonly IConfirmation _confirmation;
+
+        public Confirmation(IConfirmation confirmation)
+        {
+            _confirmation = confirmation;
+        }
+
+        public IReception Reception => _confirmation.Reception;
+
+        public IConfirmationState State => _confirmation.State;
+
+        public Task AddAsync(IGood goodToAdd, int quantity)
+        {
+            return _confirmation.AddAsync(goodToAdd, quantity);
+        }
+
+        public Task RemoveAsync(IGood goodToRemove, int quantity)
+        {
+            return _confirmation.RemoveAsync(goodToRemove, quantity);
+        }
+
+        public Task ClearAsync()
+        {
+            return _confirmation.ClearAsync();
+        }
+
+        public Task CommitAsync()
+        {
+            return _confirmation.CommitAsync();
+        }
+
+        public abstract Task<List<IGoodConfirmation>> ToListAsync();
+    }
+}
--- src/(Receptions)/(Confirmation)/NotConfirmedOnly.cs
+++ src/(Receptions)/(Confirmation)/NotConfirmedOnly.cs
@@ -4,40 +4,17 @@ using Warehouse.Core.Goods;
 
 namespace Warehouse.Core.Receptions
 {
-    public class NotConfirmedOnly : IConfirmation
+    public class NotConfirmedOnly : Confirmation
     {
         private readonly IConfirmation _confirmation;
 
         public NotConfirmedOnly(IConfirmation confirmation)
+            : base(confirmation)
         {
             _confirmation = confirmation;
         }
 
-        public IReception Reception => _confirmation.Reception;
-
-        public IConfirmationState State => _confirmation.State;
-
-        public Task AddAsync(IGood goodToAdd, int quantity)
-        {
-            return _confirmation.AddAsync(goodToAdd, quantity);
-        }
-
-        public Task RemoveAsync(IGood goodToRemove, int quantity)
-        {
-            return _confirmation.RemoveAsync(goodToRemove, quantity);
-        }
-
-        public Task ClearAsync()
-        {
-            return _confirmation.ClearAsync();
-        }
-
-        public Task CommitAsync()
-        {
-            return _confirmation.CommitAsync();
-        }
-
-        public async Task<List<IGoodConfirmation>> ToListAsync()
+        public override async Task<List<IGoodConfirmation>> ToListAsync()
         {
             var notConfirmedOnly = new List<IGoodConfirmation>();
             var goodConfirmations = await _confirmation.ToListAsync();
