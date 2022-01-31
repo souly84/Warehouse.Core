01c05c1138d387b3280883245cf63df5ba75625a
Denis <zhukovskydenis@gmail.com>
2022-01-31 23:09:21 +0300

    update unit tests

---
+++ tests/MockSupplierTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xunit;
+
+namespace Warehouse.Core.Tests
+{
+    public class MockSupplierTests
+    {
+        [Fact]
+        public void FindByReceptionDate()
+        {
+            Assert.True(
+                new MockSupplier(
+                    new MockReception(DateTime.Now.AddDays(-2))
+                ).Equals(DateTime.Now.AddDays(-2))
+            );
+        }
+
+        [Fact]
+        public void DoesNotFindByOtherReceptionDate()
+        {
+            Assert.False(
+                new MockSupplier(
+                    new MockReception(DateTime.Now.AddDays(-2))
+                ).Equals(DateTime.Now.AddDays(2))
+            );
+        }
+    }
+}
