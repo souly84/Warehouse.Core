b2b6d638cd72397ffb7dca348454500b125f08b7
Denis <zhukovskydenis@gmail.com>
2022-01-31 23:08:47 +0300

    mock supplier supports reception date equals with

--- src/Warehouse.Core/(Suppliers)/ISupplier.cs
+++ src/Warehouse.Core/(Suppliers)/ISupplier.cs
@@ -1,4 +1,7 @@
-﻿using MediaPrint;
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using MediaPrint;
 
 namespace Warehouse.Core
 {
@@ -16,13 +19,14 @@ namespace Warehouse.Core
         {
         }
 
-        public MockSupplier(string supplierName) : this(
-            supplierName,
-            new MockReception(
-                new MockReceptionGood("1", 2, "123456789"),
-                new MockReceptionGood("2", 2, "123456780"),
-                new MockReceptionGood("3", 2, "123456781"),
-                new MockReceptionGood("4", 2, "123456782")
+        public MockSupplier(string supplierName) :
+            this(
+                supplierName,
+                new MockReception(
+                    new MockReceptionGood("1", 2, "123456789"),
+                    new MockReceptionGood("2", 2, "123456780"),
+                    new MockReceptionGood("3", 2, "123456781"),
+                    new MockReceptionGood("4", 2, "123456782")
             )
         )
         {
@@ -46,11 +50,38 @@ namespace Warehouse.Core
 
         public IEntities<IReception> Receptions { get; }
 
+        public override bool Equals(object? obj)
+        {
+            return ReferenceEquals(this, obj)
+                || (obj is MockSupplier supplier && _supplierName == supplier._supplierName)
+                || (obj is string suppllierName && suppllierName == _supplierName)
+                || (obj is DateTime receptionDate && OneOfReceptionsDate(receptionDate));
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(_supplierName, Receptions);
+        }
+
         public void PrintTo(IMedia media)
         {
             media
                 .Put("name", _supplierName)
                 .Put("receptions", Receptions);
+        }
+
+        private bool OneOfReceptionsDate(DateTime dateTime)
+        {
+            var receptions = Receptions.ToListAsync().RunSync();
+            foreach (var reception in receptions)
+            {
+                if (reception.Equals(dateTime))
+                {
+                    return true;
+                }
+            }
+
+            return false;
         }
     }
 }
