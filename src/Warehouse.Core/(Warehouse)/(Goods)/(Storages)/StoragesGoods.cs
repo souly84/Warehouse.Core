6cfef054271586b16c12718540600a79bab3b181
Denis <zhukovskydenis@gmail.com>
2022-02-01 21:14:28 +0300

    mock warehouse declaration improvements

---
+++ src/Warehouse.Core/(Warehouse)/(Goods)/(Storages)/StoragesGoods.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Warehouse.Core
+{
+    public class StoragesGoods : IEntities<IWarehouseGood>
+    {
+        private readonly IFilter _filter;
+        private readonly IEnumerable<IStorage> _storages;
+
+        public StoragesGoods(params IStorage[] storages)
+            : this((IEnumerable<IStorage>)storages)
+        {
+        }
+
+        public StoragesGoods(IEnumerable<IStorage> storages)
+            : this(new EmptyFilter(), storages)
+        {
+        }
+
+        public StoragesGoods(IFilter filter, params IStorage[] storages)
+            : this(filter, (IEnumerable<IStorage>)storages)
+        {
+        }
+
+        public StoragesGoods(IFilter filter, IEnumerable<IStorage> storages)
+        {
+            _filter = filter;
+            _storages = storages;
+        }
+
+        public async Task<IList<IWarehouseGood>> ToListAsync()
+        {
+            var goodsList = new List<IWarehouseGood>();
+            foreach (var storage in _storages)
+            {
+                var storageGoods = await storage.Goods.ToListAsync();
+                goodsList.AddRange(storageGoods.Where(good => _filter.Matches(good)));
+            }
+
+            return goodsList;
+        }
+
+        public IEntities<IWarehouseGood> With(IFilter filter)
+        {
+            return new StoragesGoods(filter, _storages);
+        }
+    }
+}
---
+++ tests/MockWarehouseTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Warehouse.Core.Tests
+{
+    public class MockWarehouseTests
+    {
+        [Fact]
+        public async Task StoragesGoodsAreCombined()
+        {
+            Assert.Equal(
+                6,
+                (await new MockWarehouse(
+                    new MockStorage(
+                        new MockWarehouseGood("1", 4),
+                        new MockWarehouseGood("2", 4)
+                    ),
+                    new MockStorage(
+                        new MockWarehouseGood("1", 4),
+                        new MockWarehouseGood("3", 4)
+                    ),
+                    new MockStorage(
+                        new MockWarehouseGood("1", 4),
+                        new MockWarehouseGood("4", 4)
+                    )
+                ).Goods.ToListAsync())
+                 .Count
+            );
+        }
+    }
+}
