796dc8518805fc820070baeb9335a48cdd456ec5
Denis Zhukovski <36443640+DenisZhukovsky@users.noreply.github.com>
2022-02-02 10:06:47 +0300

    improve mock storage initialization with goods (#42)

---
+++ src/Warehouse.Core/(Core)/Entities/ComposedEntities.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Warehouse.Core
+{
+    public class ComposedEntities<T> : IEntities<T>
+    {
+        private readonly IEnumerable<IEntities<T>> _multiEntities;
+
+        public ComposedEntities(params IEntities<T>[] multiEntities)
+            : this((IEnumerable<IEntities<T>>)multiEntities)
+        {
+        }
+
+        public ComposedEntities(IEnumerable<IEntities<T>> multiEntities)
+        {
+            _multiEntities = multiEntities;
+        }
+
+        public async Task<IList<T>> ToListAsync()
+        {
+            var result = new List<T>();
+            foreach (var entities in _multiEntities)
+            {
+                result.AddRange(await entities.ToListAsync());
+            }
+            return result;
+        }
+
+        public IEntities<T> With(IFilter filter)
+        {
+            return new ComposedEntities<T>(
+                _multiEntities.Select(entities => entities.With(filter))
+            );
+        }
+    }
+}
---
+++ src/Warehouse.Core/(Warehouse)/(Goods)/(Storages)/ComposedStorages.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Warehouse.Core
+{
+    public class ComposedStorages : IStorages
+    {
+        private readonly IEnumerable<IStorages> _multiStorages;
+
+        public ComposedStorages(params IStorages[] multiStorages)
+            : this((IEnumerable<IStorages>)multiStorages)
+        {
+        }
+
+        public ComposedStorages(IEnumerable<IStorages> multiStorages)
+        {
+            _multiStorages = multiStorages;
+        }
+
+        public IEntities<IStorage> PutAway => new ComposedEntities<IStorage>(
+            _multiStorages.Select(entities => entities.PutAway)
+        );
+
+        public IEntities<IStorage> Race => new ComposedEntities<IStorage>(
+            _multiStorages.Select(entities => entities.Race)
+        );
+
+        public IEntities<IStorage> Reserve => new ComposedEntities<IStorage>(
+            _multiStorages.Select(entities => entities.Reserve)
+        );
+
+        public async Task<IList<IStorage>> ToListAsync()
+        {
+            var result = new List<IStorage>();
+            foreach (var storages in _multiStorages)
+            {
+                result.AddRange(await storages.ToListAsync());
+            }
+            return result;
+        }
+
+        public IEntities<IStorage> With(IFilter filter)
+        {
+            return new ComposedStorages(
+                _multiStorages.Select(entities => (IStorages)entities.With(filter))
+            );
+        }
+    }
+}
